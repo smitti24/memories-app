979219b7e253fa31a8eb8489710774b1ae575c37
Andre Smith <andre.frank.smith@gmail.com>
2022-08-31 08:59:34 +0200

    Initial commit

---
+++ server/controllers/posts.js
@@ -0,0 +1,29 @@
+import PostsMessage from "../models/postsMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostsMessage.find();
+
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const body = req.body;
+
+  const newPost = new PostsMessage(body);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+
+  res.send("Post Creation");
+};
